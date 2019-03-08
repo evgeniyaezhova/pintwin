85954c81e66e6420df489b411f557d76fc9d4c18
Evgeniya Ezhova <evgeniyaezhova@pursuit.org>
2019-03-08 02:47:15 -0500

    displays pins for single user

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -5,9 +5,25 @@ import "./css/UserProfile.css"
 class UserProfile extends React.Component {
 
   componentDidMount(){
-
-    this.props.fetchUserProfile(this.props.match.params.id)
+    this.props.fetchUserProfile(this.props.match.params.id);
+    this.props.fetchAllPins()
   }
+
+  displayUserPins(){
+
+    let imagesReturned;
+    let userId = this.props.user.id;
+    return (imagesReturned = this.props.pins.map((pin, i) => {
+      if(pin.user_id === userId){
+      return (
+        <div key={i} className="pins" >
+        <Link to={"/pins/" + pin.id}>
+        <img src={pin.pic_url} alt="pinshouldbehere" id="pinImg" />
+        </Link>
+        </div>
+    )}
+  }))
+}
 
   render(){
 
@@ -15,7 +31,12 @@ class UserProfile extends React.Component {
       return (
         <div>
           <div className="userDivParent">
+            <div className="usernameWrapper">
             <h1 className="username">{this.props.user.username}</h1>
+            </div>
+            <div className="pinDisplayWrapper">
+            <div className="pinDisplay">{this.displayUserPins()}</div>
+            </div>
           </div>
         </div>
       )
--- frontend/src/components/UserProfileContainer.js
+++ frontend/src/components/UserProfileContainer.js
@@ -1,18 +1,21 @@
 import UserProfile from "./UserProfile";
 import { connect } from "react-redux";
 import { fetchUserProfile } from "../actions/actionUser";
+import { fetchAllPins } from "../actions/actionPins";
 
 const mapStateToProps = (state, ownProps) => {
 
   return {
-    user: state.user[ownProps.match.params.id]
+    user: state.user[ownProps.match.params.id],
+    pins: Object.values(state.pins)
   };
 };
 
 const mapDispatchToProps = dispatch => {
 
   return {
-    fetchUserProfile: (id) => dispatch(fetchUserProfile(id))
+    fetchUserProfile: (id) => dispatch(fetchUserProfile(id)),
+    fetchAllPins: () => dispatch(fetchAllPins())
   };
 };
 
